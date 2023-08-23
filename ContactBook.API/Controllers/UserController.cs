be20ff85d41142c1cb4f41b5a6cb3c479b41ca3d
pecomen <ortizpeace@gmail.com>
2023-08-23 13:06:09 +0100

    initial commit

--- ContactBook.API/Controllers/UserController.cs
+++ ContactBook.API/Controllers/UserController.cs
@@ -15,7 +15,7 @@ namespace ContactBook.Controllers
         }
 
         [HttpPost("create")]
-        public async Task<IActionResult> CreateUser([FromBody]AddUserDto model)
+        public async Task<IActionResult> CreateUser([FromBody] AddUserDto model)
         {
             if (ModelState.IsValid)
             {
@@ -26,9 +26,18 @@ namespace ContactBook.Controllers
                     PhoneNumber = model.PhoneNumber,
                     UserName = model.Email,
                 };
-                var identityResult = await _userManager.CreateAsync(user, model.password) ;
+                var identityResult = await _userManager.CreateAsync(user, model.password);
                 if (identityResult.Succeeded)
                 {
+                    var result = await _userManager.CreateAsync(user, "regular");
+                    if (!result.Succeeded)
+                    {
+                        foreach (var err in result.Errors)
+                        {
+                            ModelState.AddModelError(err.Code, err.Description);
+                        }
+                        return BadRequest(ModelState);
+                    }
                     var userToReturn = new ReturnUserDto
                     {
                         Id = user.Id,
@@ -44,57 +53,129 @@ namespace ContactBook.Controllers
                 {
                     ModelState.AddModelError(err.Code, err.Description);
                 }
-               
+
             }
             return BadRequest(ModelState);
         }
-        [HttpGet("get-all")]
-        public IActionResult GetAll()
-        {
-            var  users = _userManager.Users.ToList();
-            var usersToReturn = new List<ReturnUserDto>();
-            if(users.Any())
-            {
-                foreach (var user in users)
-                {
-                    usersToReturn.Add(new ReturnUserDto
-                    {
-                        Id = user.Id,
-                        FirstName = user.FirstName,
-                        LastName = user.LastName,
-                        PhoneNumber = user.PhoneNumber,
-                        Email = user.Email,
-
-                    });
-                }
-            }
-            return Ok(usersToReturn);
-        }
-
-
-
-        [HttpGet("single/{id}")]
-        public async Task<IActionResult> GetSingle(string  id)
-        {
-            var user = await  _userManager.FindByIdAsync(id);
-            
-            if (user != null)
-            {
-                   var userToReturn = new ReturnUserDto
-                  {
-             
-                        Id = user.Id,
-                        FirstName = user.FirstName,
-                        LastName = user.LastName,
-                        PhoneNumber = user.PhoneNumber,
-                        Email = user.Email,
-
-                    };
-                return Ok(userToReturn);
-            }
-            return NotFound($"No user was found with id: {id}");
-            
-            
-        }
     }
 }
+    
+        //[HttpGet("get-all")]
+        //public IActionResult GetAll()
+        //{
+        //    var  users = _userManager.Users.ToList();
+        //    var usersToReturn = new List<ReturnUserDto>();
+        //    if(users.Any())
+        //    {
+        //        foreach (var user in users)
+        //        {
+        //            usersToReturn.Add(new ReturnUserDto
+        //            {
+        //                Id = user.Id,
+        //                FirstName = user.FirstName,
+        //                LastName = user.LastName,
+        //                PhoneNumber = user.PhoneNumber,
+        //                Email = user.Email,
+
+        //            });
+        //        }
+        //    }
+        //    return Ok(usersToReturn);
+        //}
+
+
+
+        //[HttpGet("single/{id}")]
+        //public async Task<IActionResult> GetSingle(string  id)
+        //{
+        //    var user = await  _userManager.FindByIdAsync(id);
+            
+        //    if (user != null)
+        //    {
+        //           var userToReturn = new ReturnUserDto
+        //          {
+             
+        //                Id = user.Id,
+        //                FirstName = user.FirstName,
+        //                LastName = user.LastName,
+        //                PhoneNumber = user.PhoneNumber,
+        //                Email = user.Email,
+
+        //            };
+        //        return Ok(userToReturn);
+        //    }
+        //    return NotFound($"No user was found with id: {id}");
+            
+            
+        //}
+
+
+//        [HttpPost("add-user-roles")]
+//        public async Task<IActionResult> AddUserRole([FromBody] AddUserRoleDto model)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                var user = new AppUser
+//                {
+//                    FirstName = model.FirstName,
+//                    LastName = model.LastName,
+//                    PhoneNumber = model.PhoneNumber,
+//                    UserName = model.Email,
+//                };
+//                var identityResult = await _userManager.CreateAsync(user, model.Password);
+//                if (identityResult.Succeeded)
+//                {
+//                    var userToReturn = new ReturnUserDto
+//                    {
+//                        Id = user.Id,
+//                        FirstName = user.FirstName,
+//                        LastName = user.LastName,
+//                        PhoneNumber = user.PhoneNumber,
+//                        Email = user.Email,
+//                    };
+//                    return Ok(userToReturn);
+//                }
+
+//                foreach (var err in identityResult.Errors)
+//                {
+//                    ModelState.AddModelError(err.Code, err.Description);
+//                }
+
+//            }
+
+//            if (ModelState.IsValid)
+//            {
+//                var user = await _userManager.FindByIdAsync(model.UserId);
+//                if (user == null)
+//                    return NotFound($"Could not find user with Id: {model.UserId}");
+
+//                var result = await _userManager.AddToRoleAsync(user, model.Role);
+//                if(!result.Succeeded)
+//                {
+//                    foreach(var err in result.Errors)
+//                    {
+//                        ModelState.AddModelError(err.Code, err.Description);
+//                    }
+//                }
+//                return Ok("Role added to user!");
+
+
+//            }
+//            return BadRequest(ModelState);
+
+
+//        }
+     
+
+//        [HttpPost("get-user-roles/{userId")]
+//        public async Task<IActionResult> GetUserRoles(string userId)
+//        {
+//            var user = await _userManager.FindByIdAsync(userId);
+//            if (user == null)
+//                return NotFound($"could not find user with Id: {userId}");
+
+//            var userRoles = await _userManager.GetRolesAsync(user);
+//            return Ok(userRoles);
+//        }
+//    }
+//}
---
+++ ContactBook.API/DTOs/AddUserRoleDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactBook.DTOs
+{
+
+    public class AddUserRoleDto
+    {
+        [Required]
+        public string UserId { get; set; } = "";
+
+
+        [Required]
+        public string Role { get; set; } = "";
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+        public string PhoneNumber { get; internal set; }
+        public string Email { get; internal set; }
+        public string Password { get; internal set; }
+    }
+}
